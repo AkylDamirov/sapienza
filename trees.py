c33df30778867ff2c38b8f569a115fd9c24b70bc
Akyl <akyl@gmail.com>
2025-01-10 19:09:20 +0100

    working with trees

--- trees.py
+++ trees.py
@@ -78,6 +78,61 @@ n2.add_child(n4)
 n2.add_child(n5)
 
 
-
-
-
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.right = self.left = None
+#
+# class Tree:
+#     def __init__(self):
+#         self.root = None
+#
+#     def __find(self, node, parent, value):
+#         if node is None:
+#             return None, parent, False
+#         if value == node.data:
+#             return node, parent, True
+#         if value < node.data:
+#             if node.left:
+#                 return self.__find(node.left, node, value)
+#
+#         if value > node.data:
+#             if node.right:
+#                 return self.__find(node.right, node, value)
+#         return node, parent, False
+#
+#
+#     def append(self, obj):
+#         if self.root is None:
+#             self.root = obj
+#             return obj
+#
+#         s, p, fl_find = self.__find(self.root, None, obj.data)
+#
+#         if not fl_find and s:
+#             if obj.data < s.data:
+#                 s.left = obj
+#             else:
+#                 s.right = obj
+#
+#     def show_tree(self, node):
+#         if node is None:
+#             return
+#
+#         self.show_tree(node.left)
+#         print(node.data)
+#         self.show_tree(node.right)
+#
+#     def __del_leaf(self, s, p):
+#         if p.left == s:
+#             p.left = None
+#         elif p.right == s:
+#             p.right = None
+#
+# v = [10, 5, 7, 16, 13, 2, 20]
+#
+# t = Tree()
+# for x in v:
+#     t.append(Node(x))
+#
+# t.show_tree(t.root)
